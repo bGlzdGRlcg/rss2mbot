bba17a86711b707bb39c5ed59efd49437a56adce
bGlzdGRlcg <listder@outlook.com>
2025-03-17 14:19:39 +0800

    fix: help

--- bots/config.go
+++ bots/config.go
@@ -8,13 +8,13 @@ import (
 
 const (
 	Start_Text = `
-	/getid username (获取Mastodon ID)
-	/getinfo (获取账号信息)
-	/bind Mastodon_ID (绑定Mastodon账号)
-	/sub rss_url (订阅rss)
-	/unsub index (取消订阅rss)
-	/getsublist (获取订阅列表)
-	/ping (200 ok)
+/getid username (获取Mastodon ID)
+/getinfo (获取账号信息)
+/bind Mastodon_ID (绑定Mastodon账号)
+/sub rss_url (订阅rss)
+/unsub index (取消订阅rss)
+/getsublist (获取订阅列表)
+/ping (200 ok)
 	`
 	sublim = 5
 )
